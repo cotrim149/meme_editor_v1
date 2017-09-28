4e085be38c42ae85e45745906eb19077131d9108
Victor Cotrim <victor.cotrim@altran-ais.com.br>
2017-09-28 08:09:36 -0300

    Adding initial files

---
+++ meme_editor_v1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  meme_editor_v1
+//
+//  Created by Victor de Lima on 28/09/17.
+//  Copyright © 2017 Victor de Lima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
